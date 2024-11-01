a96f1f57c6370b55ab52f8fc6084b1e94f309a0c
cristian69 <medialunacesar@gmail.com>
2024-10-31 22:48:17 -0500

    agregue un panel a principalcandidato

--- Conectalento-P3/GUI/PrincipalCandidato.Designer.cs
+++ Conectalento-P3/GUI/PrincipalCandidato.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(469, 263);
+            this.button1.Location = new System.Drawing.Point(219, 112);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -43,18 +44,26 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(191, 263);
+            this.button2.Location = new System.Drawing.Point(219, 141);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(425, 104);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(308, 208);
+            this.panel1.TabIndex = 2;
+            // 
             // PrincipalCandidato
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "PrincipalCandidato";
@@ -67,5 +76,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
