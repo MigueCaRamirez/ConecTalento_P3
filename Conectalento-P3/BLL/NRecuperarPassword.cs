374602aebac30ec1774856f9194e99112bd5a847
cristian69 <medialunacesar@gmail.com>
2024-11-06 10:28:18 -0500

    envio de correo

--- Conectalento-P3/BLL/NRecuperarPassword.cs
+++ Conectalento-P3/BLL/NRecuperarPassword.cs
@@ -12,21 +12,21 @@ namespace BLL
     {
         public bool LoginUser(string username, string password)
         {
-            // Asegúrate de que la conexión a la base de datos esté correctamente configurada
+            
             using (var connection = new SqlConnection(Conexion.conexion))
             {
                 connection.Open();
                 using (var command = new SqlCommand())
                 {
                     command.Connection = connection;
-                    command.CommandText = "SELECT COUNT(*) FROM Usuarios WHERE login = @Login AND Password = @Password"; // Asegúrate de que esto coincida con tu base de datos
+                    command.CommandText = "SELECT COUNT(*) FROM Usuarios WHERE login = @Login AND Password = @Password"; 
                     command.Parameters.AddWithValue("@Login", username);
                     command.Parameters.AddWithValue("@Password", password);
                     command.CommandType = System.Data.CommandType.Text;
 
-                    // Ejecuta la consulta y verifica si hay coincidencias
+                    
                     int result = (int)command.ExecuteScalar();
-                    return result > 0; // Retorna true si existe un usuario con esas credenciales
+                    return result > 0; 
                 }
             }
         }
